c8b66dd565303b8d603e5e605a2d9c60be07be7b
Ssandstormm <96559353+Ssandstormm@users.noreply.github.com>
2022-10-16 22:53:34 +0600

    Add files via upload

---
+++ ex/pythonnn.py
@@ -0,0 +1,74 @@
+
+from flask import Flask, request,render_template,jsonify,redirect,url_for
+import sqlite3
+
+app = Flask(__name__)
+
+
+@app.route("/")
+
+def home():
+
+    return "NFT"
+
+
+@app.route('/data/', methods = ['POST', 'GET'])
+def data():
+    return render_template('form.html')
+     
+
+
+if __name__ == "__main__":
+
+    app.run(debug=True)
+import requests
+
+
+
+
+nft = "3HcD2Zz7cpZShUbS4KTEAXfLu31Yb9zb8wcJmNB6cQsh"
+
+url = f"https://solana-gateway.moralis.io/nft/mainnet/{nft}/metadata"
+
+
+
+headers = {
+
+    "accept": "application/json",
+
+    "X-API-Key": "qoTYAokKqHK6xS5e0Zbq3qg2kh2uUzy4FfObczDjE5VdBa9Pi87PDxRl3RV54Fpc"
+
+}
+
+
+
+response = requests.get(url, headers=headers)
+
+
+
+print(response.json)
+
+@app.route('/json-data/')
+def json_data():
+    # get number of items from the javascript request
+    nitems = request.args.get('1', 2)
+    # query database
+    cursor = g.db.execute('select * from items limit ?', (1,))
+    # return json
+    return jsonify(dict(cursor.fetchall(), start=1))
+
+
+
+import psycopg2
+
+connection = psycopg2.connect(database="NFT", user="postgres", password="550215", host="localhost", port=5432)
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * from nfts")
+
+# Fetch all rows from database
+record = cursor.fetchall()
+ 
+print("Data from Database:- ", record)
+connection.close()
