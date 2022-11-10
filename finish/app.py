615dcae182e88fe68f1d9631b6259d4cc0c0ec62
Ssandstormm <96559353+Ssandstormm@users.noreply.github.com>
2022-11-10 15:17:44 +0600

    Add files via upload

--- finish/app.py
+++ finish/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm 
 from wtforms import StringField, PasswordField, BooleanField
@@ -6,12 +6,15 @@ from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
+import requests
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Thisissupposedtobesecret!'
 app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:550215@localhost:5432/usersofpy"
 bootstrap = Bootstrap(app)
 db = SQLAlchemy(app)
+
 login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
@@ -21,6 +24,10 @@ class User(UserMixin, db.Model):
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+class NFTS(db.Model):
+    nft_name = db.Column(db.String(), primary_key=True) 
+    url = db.Column(db.String())
+    address = db.Column(db.String())
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -76,6 +83,39 @@ def signup():
 @login_required
 def dashboard():
     return render_template('form.html', name=current_user.username)
+@app.route('/dashboard',methods = ['POST'])
+@login_required
+def dashboard2():
+    address = request.form['Name']
+    x = NFTS.query.filter_by(address=address).first()
+    if x is None:
+        url = f"https://solana-gateway.moralis.io/nft/mainnet/{address}/metadata"
+        headers = {
+            "accept": "application/json",
+            "X-API-Key": "S77RJTmiMoBbTQTEed5MExSDfHQ2HolnDEXy7GZRoo3Eah6t1YAR20dfdGIJASaT"
+        }    
+        response = requests.get(url, headers=headers)
+        info = response.text
+        i = info.find("name")+7
+        i2 = 0
+        for x in range(i, len(info)):
+            if info[x] == '"':
+                i2 = x
+                break
+        i3 = info.find("metadataUri")+14
+        i4 = 0
+        for x in range(i3, len(info)):
+            if info[x] == '"':
+                i4 = x
+                break
+        name = info[i:i2]
+        url = info[i3:i4]
+        nft = NFTS(nft_name = name, url = url, address= address)
+        db.session.add(nft)
+        db.session.commit()
+    nft = NFTS.query.filter_by(address=address).first()
+    return render_template('data.html', address = address, name = nft.nft_name , url = nft.url)
+
 
 @app.route('/logout')
 @login_required
